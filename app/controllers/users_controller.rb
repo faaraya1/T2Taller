a27f33260dea625752751ad7184d3c59410fa676
Fernando <faaraya1@uc.cl>
2017-03-30 19:50:36 -0300

    todo menos el manejo de errores

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,35 @@
 class UsersController < ApplicationController
+
+	#GET
 	def index
 		@users = User.all
 		render json: @users, status: :ok		
 	end
 
+	#PUT
 	def create
 		@user = User.new(user_params)
 		@user.save
 		render json: @user, status: :created
 	end
 
+	#GET/ID
+	def show
+		@user = User.where(id: params[:id]).first
+		render json: @user, status: :ok
+	end
+
+	#POST/ID
+	def update
+    	@user = User.where(id: params[:id]).first
+    	if @user.update(user_params)
+        	render json: @user, status: :ok
+    	else
+        	render json: { errors: @user.errors }, status: 422
+    	end
+  	end
+
+	#DELETE
 	def destroy
 		@user = User.where(id: params[:id]).first
 		if @user.destroy
