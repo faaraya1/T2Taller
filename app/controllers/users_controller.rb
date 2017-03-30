fd50bb4263aa8e72381bd4e945e77629b746ba3d
Fernando <faaraya1@uc.cl>
2017-03-30 16:04:18 -0300

    Initialize repo

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+	def index
+		@users = User.all
+		render json: @users, status: :ok		
+	end
+
+	def create
+		@user = User.new(user_params)
+		@user.save
+		render json: @user, status: :created
+	end
+
+	def destroy
+		@user = User.where(id: params[:id]).first
+		if @user.destroy
+			head(:ok)
+		else
+			head(:unprocessable_entity)
+		end		
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:nombre, :apellido, :usuario, :twitter )
+	end
+
+end
